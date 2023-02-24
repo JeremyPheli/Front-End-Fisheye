8e111f609b0e205abd820089546c1208c89c3daa
Jeremy <jeremy.phelippeau.91@gmail.com>
2023-02-24 12:11:35 +0100

    init commit

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,16 @@
 async function getPhotographers() {
   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  let photographers = [
-    {
-      name: "Jean Luc",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "François",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
+  fetch("data/photographers.json").then((res) => {
+    if (res.ok) {
+      res.json().then((data) => {
+        console.log(data);
+      });
+    } else {
+      console.log("Erreur");
+    }
+  });
+
   // et bien retourner le tableau photographers seulement une fois récupéré
   return {
     photographers: [...photographers, ...photographers, ...photographers],
